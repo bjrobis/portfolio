08c97b11a1f8fe1afa31bd11be1217216f919c09
Brandon Robison <bjrobis@gmail.com>
2023-02-03 14:03:32 -0700

    changed port

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use((err, req, res, next) => {
 });
 
 //3000 is the port number
-app.listen(`0.0.0.0:$PORT`, () => {
-    console.log('The application is running on localhost:3000')
+app.listen(PORT, () => {
+    console.log(`The application is running on ${PORT}`)
 }); 
